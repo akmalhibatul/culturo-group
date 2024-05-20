c7dad527e465630022610852420bb89221df9a9e
Hibatul Akmal <akmalhibatul1@gmail.com>
2024-05-20 16:40:31 +0700

    upload pertama

---
+++ index.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Culturo Group</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+
+<body>
+    <section class="navbar mb-5">
+        <div class="container-fluid">
+            <div class="row">
+                <img src="assets/logo.png" class="img-fluid logo-atas mx-5">
+            </div>
+        </div>
+    </section>
+    <section class="hero mt-4">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-7">
+                    <h1 class="ubuntu-medium mx-5 f-hero mb-4">A Symphony <br> of Solutions</h1>
+                    <p class="ubuntu-regular mx-5 f-paragraf">Our holding company fosters a diverse ecosystem of <br> expertise, empowering businesses and individuals <br> across various sectors.</p>
+                    <a href="" class="btn btn-primary mx-5 button-contact">Contact Us</a>
+                </div>
+                <div class="col-md-5">
+                    <img src="assets/hero-bg.png" class="img-fluid hero-bg rounded mx-auto d-block">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="about mb-5">
+        <div class="container-fluid">
+            <div class="row mx-5">
+                <div class="col-md-5">
+                    <img src="assets/about-image.png" class="img-fluid about-image">
+                </div>
+                <div class="col-md-7">
+                    <h2 class="ubuntu-medium" style="font-size: 60px;">Fostering Growth and <br> Well-being</h2>
+                    <p class="ubuntu-regular" style="font-size: 24px;text-align: justify;">We are a holding company that unites a dynamic network of innovative companies specializing in various sectors. Our core strength lies in fostering collaboration and synergy between these companies, allowing us to offer a comprehensive range of solutions that empower businesses and individuals to thrive.</p>
+                </div>
+            </div>
+            <div class="row mx-5 mt-5">
+                <div class="col-md-5">
+                    <h4 class="ubuntu-medium mb-4" style="font-size: 32px;line-height: 36px;">To become a leading force in <br> driving growth and well-being <br> across diverse industries.</h4>
+                    <p class="ubuntu-regular" style="font-size: 20px;">We envision a future where businesses flourish through innovative marketing strategies and individuals achieve optimal health through cutting-edge healthcare <br> solutions.</p>
+                </div>
+                <div class="col-md-7">
+                    <h2 class="ubuntu-medium" style="font-size: 60px;">Fostering Growth and <br> Well-being</h2>
+                    <p class="ubuntu-regular" style="font-size: 24px;text-align: justify;">We are a holding company that unites a dynamic network of innovative companies specializing in various sectors. Our core strength lies in fostering collaboration and synergy between these companies, allowing us to offer a comprehensive range of solutions that empower businesses and individuals to thrive.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</html>
